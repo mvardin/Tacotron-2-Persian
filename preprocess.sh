33842e2fe0d1adf1dfc32311640ff348e8695d01
Ardin <mvardin@gmail.com>
2021-08-08 14:18:11 +0430

    sync with local

--- preprocess.sh
+++ preprocess.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-DATASET_PATH="/home/mvardin/Tacotron-2-Persian/fa"
-OUTPUT_PATH="/home/mvardin/Tacotron-2-Persian/ppoutput"
-CONFIG_PATH="/home/mvardin/Tacotron-2-Persian"
-NUM_WORKERS=10
+DATASET_PATH="/home/mvardin/Tacotron-2-Persian/Corpus/fa"
+OUTPUT_PATH="/home/mvardin/Workarea/ppoutput"
+CONFIG_PATH="/home/mvardin/Workarea/Tacotron-2-Persian"
+NUM_WORKERS=32
+SPEACKER="045313dea664e9b5cfc4bd4b0211b1517740f736ca6204255180d0216644a3b23da079514e084f3562fb92e41f853d14c82bb8c8b9241aa190c7509133573655"
 
 python -m tac2persian.data_preprocessing.preprocess_commonvoice_fa --dataset_path="$DATASET_PATH" \
                                                                    --output_path="$OUTPUT_PATH" \
                                                                    --config_path="$CONFIG_PATH" \
-                                                                   --num_workers="$NUM_WORKERS"
+                                                                   --num_workers="$NUM_WORKERS" \
+                                                                   --speaker="$SPEACKER" \
--- train.sh
+++ train.sh
@@ -5,4 +5,4 @@ full_path=$0
 CONFIG_PATH=$(dirname "$full_path")
 echo $CONFIG_PATH
 
-python -m tac2persian.train --config_path="$CONFIG_PATH"
+python -m tac2persian.train --config_path="$CONFIG_PATH" --output_path="/home/mvardin/Workarea/Tacotron-2-Persian/" --checkpoint_path="" 
